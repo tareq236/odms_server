2774fafd327982604bcc13902f65e4ce7e6e8443
najmulislamnajim <ninbd17@gmail.com>
2024-11-11 10:32:41 +0600

    [ADD] get API for return reason list

--- sap_app/views.py
+++ sap_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .constants import ReturnReason
+from .serializers import ReturnReasonSerializer
 
 # Create your views here.
+class ReturnReasonView(APIView):
+    def get(self,request):
+        return_reasons= [{'code': choice.value, 'reason': choice.label} for choice in ReturnReason]
+        serializer = ReturnReasonSerializer(return_reasons, many=True)
+        return Response(serializer.data)
