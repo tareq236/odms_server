10e3a00a41301bdeab74f72ae4949965b740fe34
najmulislamnajim <ninbd17@gmail.com>
2024-11-26 11:03:58 +0600

    [UPDATE] define route for dashboard info

--- report_app/urls.py
+++ report_app/urls.py
@@ -3,5 +3,6 @@ from report_app import views
 
 urlpatterns = [
     path('dashboard/<int:sap_id>', views.dashboard_report),
+    path('dashboard/info/<int:sap_id>', views.dashboard_info),
     path('activity_for_map/<int:sap_id>/<str:date>', views.activity_for_map),
 ]
