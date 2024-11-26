dd7d0920cfa0df2f63a6c5cdf853b727fe73be7e
najmulislamnajim <ninbd17@gmail.com>
2024-11-26 10:33:09 +0600

    [UPDATE] define route for overdue collect

--- overdue_app/urls.py
+++ overdue_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import overdue_list
+from .views import overdue_list,collect_overdue
 
 urlpatterns = [
     path('list/<int:da_code>',overdue_list,name='overdue_list'),
+    path('collect',collect_overdue,name='collect_overdue'),
 ]
