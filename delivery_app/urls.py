9020c03e2c4fde6e3c7fef82a14720fda9daa258
najmulislamnajim <ninbd17@gmail.com>
2025-02-12 10:57:49 +0600

    [UPDATE] save view

--- delivery_app/urls.py
+++ delivery_app/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from delivery_app import views
+from delivery_app import views,views2
 from . views3 import delivery_list
 
 urlpatterns = [
     path('list/<int:sap_id>', views.delivery_list),
     # path('v2/list/<int:sap_id>', views.delivery_list_v2),
     path('v2/list/<int:sap_id>', delivery_list),
-    path('save', views.delivery_save),
+    path('save', views2.delivery_save),
 ]
