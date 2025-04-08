adb73239dab47bd640afbb8d1ed168ba372a8a08
najmulislamnajim <najmulislamru@gmail.com>
2025-04-08 13:04:57 +0600

    update urls

--- conveyance_app/urls.py
+++ conveyance_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import TransportModeListView, ConveyanceListView, StartJourneyView, EndJourneyView
+from .views import TransportModeListView, ConveyanceListView, StartJourneyView, EndJourneyView, DaMovementInfoView
 
 urlpatterns = [
     path('list', ConveyanceListView.as_view(), name='conveyance_list'),
     path('start', StartJourneyView.as_view(), name='start_journey'),
     path('end/<int:id>', EndJourneyView.as_view(), name='end_journey'),
     path('transport_modes', TransportModeListView.as_view(), name='transport_mode_list'),
+    path('movement_info', DaMovementInfoView.as_view(), name='da_movement_info')
 ]
