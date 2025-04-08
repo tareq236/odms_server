b9f0c39584d6f9a30851378c39105a4189924126
najmulislamnajim <najmulislamru@gmail.com>
2025-04-08 12:16:15 +0600

    add time_duration & distance in conveyance model

--- conveyance_app/models.py
+++ conveyance_app/models.py
@@ -14,6 +14,8 @@ class ConveyanceModel(models.Model):
     transport_mode = models.TextField(null=True, blank=True) 
     transport_cost = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     journey_status = models.CharField(max_length=20)
+    time_duration = models.IntegerField(null=True, blank=True) # in miliseconds
+    distance = models.IntegerField(null=True, blank=True) # in meters
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
 
